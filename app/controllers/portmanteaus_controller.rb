c4a2d683b56cb14efd988a69445b94183cb2638e
MattyZ <mzickel@gmail.com>
2020-05-31 18:15:42 -0400

    new and create for portfolios

--- app/controllers/portmanteaus_controller.rb
+++ app/controllers/portmanteaus_controller.rb
@@ -2,4 +2,21 @@ class PortmanteausController < ApplicationController
   def index
     @portfolio_items= Portmanteau.all
   end
+
+  def new
+#   @portfolio = Portmanteau.new
+   @portmanteau = Portmanteau.new
+
+  end
+  def create
+    #@portmanteau= Portmanteau.new(portmanteau_params)
+    @portmanteau= Portmanteau.new(params.require(:portmanteau).permit(:title, :subtitle, :body))
+    respond_to do |format|
+      if @portmanteau.save
+        format.html { redirect_to portmanteaus_path, notice: 'Portfolio item was successfully created.' }
+      else
+        format.html { render :new }
+      end
+    end
+  end
 end
