18e36fb73c0c1fa39a45264184df773c7cd16805
MattyZ <mzickel@gmail.com>
2020-06-01 14:14:47 -0400

    added custom routes for about contact pages

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,17 @@
 Rails.application.routes.draw do
   resources :portmanteaus
-  get 'pages/home'
-  get 'pages/about'
-  get 'pages/contact'
+  # static pages
+  # get 'pages/home'
+
+  # url, controller#action'
+  get 'about', to: 'pages#about'
+  # get 'pages/contact'..orig, replaced by below so user doesn't requre added pages to view content
+  get 'contact', to: 'pages#contact'
+  # your can add any custom url
+  get 'about-me', to: 'pages#about'
+
   resources :blogs
+  #this is the home page of the application
+  root to: 'pages#home'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
