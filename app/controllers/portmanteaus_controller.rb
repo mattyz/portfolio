d82017521a7f80b6c376056a2acdd5ff5fbd1aa4
MattyZ <mzickel@gmail.com>
2020-06-01 11:05:27 -0400

    added edit and update functionality

--- app/controllers/portmanteaus_controller.rb
+++ app/controllers/portmanteaus_controller.rb
@@ -19,4 +19,20 @@ class PortmanteausController < ApplicationController
       end
     end
   end
+
+  def edit
+    @portmanteau = Portmanteau.find(params[:id])
+    # puts @portmanteau.inspect
+  end
+
+  def update
+    @portmanteau= Portmanteau.new(params.require(:portmanteau).permit(:title, :subtitle, :body))
+    respond_to do |format|
+      if @portmanteau.update(params.require(:portmanteau).permit(:title, :subtitle, :body))
+        format.html { redirect_to portmanteaus_path, notice: 'Portfolio was successfully updated.' }
+      else
+        format.html { render :edit }
+      end
+    end
+  end
 end
