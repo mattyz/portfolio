51e0d7a1ba53cb67d24be95f531f4af20224842c
mattyz <m.zickel@gmail.com>
2020-06-02 12:08:25 -0400

    Portfolio feature (#2)
    * seeds db sample data
    
    * added portfolios, fixed db seed, included the default template scaffoled for index
    
    * new and create for portfolios
    
    * added edit and update functionality
    
    * added new and edit button for port
    
    * implemented show functionailty
    
    * added delete to show page and controllor for portfolio
    
    * added custom routes for about contact pages
    
    * customize routes for portfolio
    
    * integration friendly id for blogs
    
    * updated enum blog status
    
    * toggle draft published status
    
    Co-authored-by: MattyZ <mzickel@gmail.com>

--- app/controllers/portmanteaus_controller.rb
+++ app/controllers/portmanteaus_controller.rb
@@ -1,2 +1,50 @@
 class PortmanteausController < ApplicationController
+  def index
+    @portfolio_items= Portmanteau.all
+  end
+
+  def new
+#   @portfolio = Portmanteau.new
+   @portmanteau = Portmanteau.new
+
+  end
+  def create
+    #@portmanteau= Portmanteau.new(portmanteau_params)
+    @portmanteau= Portmanteau.new(params.require(:portmanteau).permit(:title, :subtitle, :body))
+    respond_to do |format|
+      if @portmanteau.save
+        format.html { redirect_to portmanteaus_path, notice: 'Portfolio item was successfully created.' }
+      else
+        format.html { render :new }
+      end
+    end
+  end
+
+  def edit
+    @portmanteau = Portmanteau.find(params[:id])
+    # puts @portmanteau.inspect
+  end
+
+  def show
+    @portmanteau = Portmanteau.find(params[:id])
+  end
+
+  def destroy
+    @portmanteau = Portmanteau.find(params[:id])
+    @portmanteau.destroy
+    respond_to do |format|
+      format.html { redirect_to portmanteaus_url, notice: 'Portfolio was successfully destroyed.' }
+    end
+  end
+
+  def update
+    @portmanteau= Portmanteau.new(params.require(:portmanteau).permit(:title, :subtitle, :body))
+    respond_to do |format|
+      if @portmanteau.update(params.require(:portmanteau).permit(:title, :subtitle, :body))
+        format.html { redirect_to portmanteaus_path, notice: 'Portfolio was successfully updated.' }
+      else
+        format.html { render :edit }
+      end
+    end
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,27 @@
 Rails.application.routes.draw do
-  resources :portmanteaus
-  get 'pages/home'
-  get 'pages/about'
-  get 'pages/contact'
-  resources :blogs
+  # resources :portmanteaus
+ # problem with resources above is the show action requires portmanteaus/id, which is plural!
+  # ergo, customize it with portmanteau/:id
+  resources :portmanteaus, except: [:show]
+  #as is used for a link_to tag on index
+  get 'portmanteau/:id', to: 'portmanteaus#show', as: 'portmanteau_show'
+  
+  # static pages
+  # get 'pages/home'
+
+  # url, controller#action'
+  get 'about', to: 'pages#about'
+  # get 'pages/contact'..orig, replaced by below so user doesn't requre added pages to view content
+  get 'contact', to: 'pages#contact'
+  # your can add any custom url
+  get 'about-me', to: 'pages#about'
+
+  resources :blogs do
+    member do
+      get :toggle_status
+    end
+  end
+  #this is the home page of the application
+  root to: 'pages#home'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
