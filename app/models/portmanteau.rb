cd92bda93c17c6c73562d9f584990d8b46701e13
MattyZ <mzickel@gmail.com>
2020-06-05 14:03:14 -0400

    custom scopes

--- app/models/portmanteau.rb
+++ app/models/portmanteau.rb
@@ -1,3 +1,11 @@
 class Portmanteau < ApplicationRecord
   validates_presence_of :title, :body, :main_image, :thumb_image
+
+  def self.activities
+    where(subtitle: 'Activities')
+  end
+ 
+  # preferred way
+  # Pormanteau.portolio_activities
+  scope :portfolio_activities, ~> {where(subtitle: 'Activities')}
 end
