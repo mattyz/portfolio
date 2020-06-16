bc23e47ab4b0d72c41d6781b6ca7fdc39448d257
MattyZ <mzickel@gmail.com>
2020-06-16 07:39:12 -0400

    fix error with params in portfolio concern

--- app/models/portmanteau.rb
+++ app/models/portmanteau.rb
@@ -16,8 +16,11 @@ class Portmanteau < ApplicationRecord
   after_initialize :set_defaults
 
   def set_defaults
-   self.main_image ||=  Placeholder.image_generator(height:"600", width: "400")
-   self.thumb_image ||=  Placeholder.image_generator(height:"350", width: "200")
+   # self.main_image ||=  Placeholder.image_generator(height:"600", width: "400")
+   # self.thumb_image ||=  Placeholder.image_generator(height:"350", width: "200")
+
+   self.main_image ||=  Placeholder.image_generator("600","400")
+   self.thumb_image ||=  Placeholder.image_generator("350", "200")
 
 #    self.main_image ||= "http://placehold.it/600x400" 
 #    self.thumb_image ||= "http://placehold.it/350x200"
