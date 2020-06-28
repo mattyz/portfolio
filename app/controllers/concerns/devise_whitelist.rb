86612228f1d91955288be5183486a2133814b3e6
MattyZ <mzickel@gmail.com>
2020-06-28 19:53:59 -0400

    enforce single responsibility principle by moving code out of app controller to module

--- app/controllers/concerns/devise_whitelist.rb
+++ app/controllers/concerns/devise_whitelist.rb
@@ -1,3 +1,4 @@
+require 'active_support/concern'
 # module helper code that is shared by other classes, more flexible, shared behavior with other classes
 # class is rails specfic
 module DeviseWhitelist
@@ -7,7 +8,7 @@ module DeviseWhitelist
     before_action :configure_permitted_parameters, if: :devise_controller?
   end
   
-  def configure_permitted_parameters·
+  def configure_permitted_parameters
     devise_parameter_sanitizer.permit(:sign_up, keys: [:name])
     devise_parameter_sanitizer.permit(:account_update, keys: [:name])
   end
