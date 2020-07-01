c8ecdadc75b6ab84e022fe93014cf42bae7b1373
MattyZ <mzickel@gmail.com>
2020-07-01 10:20:17 -0400

    sssion params and tracker for dynamic sources

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -5,4 +5,16 @@ class ApplicationController < ActionController::Base
   # message NoMethodError (undefined method `configure_permitted_parameters' for #<Devise::SessionsController:0x00007f89d0d22d08>
 # Did you mean?  configure_permitted_parameters·)
   include DeviseWhitelist
+
+  before_action :set_source
+
+
+  # TODO: refactor
+  # if params passed in add something to the view
+  # add this to the session as source
+  # in browser add ?q=twitter and key[:source} in session is mapped 
+  # to twitter
+  def set_source
+    session[:source] = params[:q] if params[:q]
+  end
 end
