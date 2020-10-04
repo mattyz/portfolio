85c619eb5d7f00d74d4dfbfc0696d997aea5e914
MattyZ <mzickel@gmail.com>
2020-10-04 11:52:42 -0400

    changes for image display

--- app/models/skill.rb
+++ app/models/skill.rb
@@ -1,5 +1,5 @@
 class Skill < ApplicationRecord
-  includes Placeholder
+#   includes Placeholder
 
   validates_presence_of :title, :percent_utilized
 
@@ -9,7 +9,8 @@ class Skill < ApplicationRecord
    # replece below with a concern 
    # self.badge||= "http://placehold.it/200x200" 
    # self.badge||=  Placeholder.image_generator(height:"200", width: "200")
-   self.badge||=  Placeholder.image_generator("200","200")
+ # carrierwave remove
+    #  self.badge||=  Placeholder.image_generator("200","200")
   end
 
 end
